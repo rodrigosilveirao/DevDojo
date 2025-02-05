bfec06835601a698996fe07a29776f9e3b3d33ba
Rodrigo Mac <rodrigocunhasilveira@MacBook-Air-de-Rodrigo.local>
2025-02-05 17:41:31 -0300

    first commit

---
+++ src/DiferencaIdade.java
@@ -0,0 +1,7 @@
+public class DiferencaIdade {
+    public static void main(String[] args) {
+        int idade1 = 100;
+        int idade2 = 30;
+        System.out.println("A diferença de idade é:" + (idade1 - idade2));
+    }
+}
