001ee3a44e3e030a41f4aa63996c6ae1a727ba58
Rodrigo Mac <rodrigocunhasilveira@MacBook-Air-de-Rodrigo.local>
2025-02-05 17:44:09 -0300

    segunda opcao de fazer

--- src/DiferencaIdade.java
+++ src/DiferencaIdade.java
@@ -2,6 +2,8 @@ public class DiferencaIdade {
     public static void main(String[] args) {
         int idade1 = 100;
         int idade2 = 30;
-        System.out.println("A diferença de idade é:" + (idade1 - idade2));
+        int diferencaIdade = idade1 - idade2;
+        System.out.println("A diferença de idade é:" + diferencaIdade);
     }
 }
+
