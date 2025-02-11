5daa6051ae421b5b78649c32d0da7244a7e826a6
Rodrigo Mac <rodrigocunhasilveira@MacBook-Air-de-Rodrigo.local>
2025-02-11 16:54:16 -0300

    avanco legal

--- src/TorneioNatacao.java
+++ src/TorneioNatacao.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TorneioNatacao {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(" Digite seu nome: ");
+        String nome = s.next();
+        System.out.println("Digite sua idade para saber qual sua categoria: ");
+        int idade = s.nextInt();
+        if (idade <= 10){
+            System.out.println(" Categoria infantil. ");
+        } else if (idade >= 11 && idade <= 15) {
+            System.out.println(" Categoria juvenil. ");
+        } else if (idade >= 16 && idade <= 19) {
+            System.out.println(nome +" com sua idade (" + idade + ") voce esta na categoria pré-adulto. ");
+        } else {
+            System.out.println(" Categoria adulto. ");
+        }
+    }
 }
